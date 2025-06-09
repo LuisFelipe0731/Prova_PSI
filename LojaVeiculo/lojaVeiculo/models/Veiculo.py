463f5bd1f683fffac6474bb1094f6ed013220ab1
Luis Felipe Rodrigues Freire <20231011110028@ifrn.local>
2025-06-09 15:33:35 -0300

    roxanne

--- LojaVeiculo/lojaVeiculo/models/Veiculo.py
+++ LojaVeiculo/lojaVeiculo/models/Veiculo.py
@@ -1,7 +1,10 @@
 from lojaVeiculo.models import *
 class Veiculo(models.Model):
     Veiculo = models.CharField(null=False, max_length=100)
-    criado_em = models.DateTimeField(auto_now_add=True)
-    alterado_em = models.DateTimeField(auto_now=True)
+    preco = models.DecimalField(max_digits=8, decimal_places=2)
+    cor = models.CharField(null=False, max_length=100)
+    descricao = models.CharField(null=False, max_length=100)
+    ano = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return '{}'.format(self. Veiculo)
